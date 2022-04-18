34c150256650dd0296243ae13404e096f9472177
Talha-crypto <60981588+Talha-crypto@users.noreply.github.com>
2022-04-18 22:10:18 +0500

    Database user data entry
    Enter data in database. Then get input and check it with database for matching entries if exists.

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,40 @@
 //Basic Validatiion for collecting data from the users
 
 const express = require("express")
+const { default: mongoose } = require("mongoose")
 const router = express.Router()
 
+const User=mongoose.model("User")
+
 router.get('/',(req,res)=>{
+    res.send("Hello World")
+})
+
+router.get('/signup',(req,res)=>{
     const {name,email,password} = req.body
     if(!email || !password || !name)
     {
         return res.status(422).json({error:"Please fill all the fields"}) // Status code for explaining type of error on console
     }
-    res.json({message:"Successfuly posted!"})
+    //Function to check email and return the results after comparing it with(frontend input) the database data
+    User.findOne({email:email})
+    .then((savedUser)=>{ // Exception handling for checking email if already exists (Then and catch methods used)
+        if(savedUser)
+        {
+            return res.status(422).json({error:"User already exist with this Email"})
+        }
+        const user =new User({
+            email,
+            password,
+            name
+        })
+        user.save()
+        .then(user=>{
+            res.json({message:"Saved Successfuly"})
+        })
+        .catch(err=>{console.log(err)})
+    })
+    .catch(err=>{console.log(err)})
 })
 
 module.exports=router
