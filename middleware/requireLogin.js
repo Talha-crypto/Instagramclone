f951a7d30cf19ac6a538527346aef5a54488bc48
Talha-crypto <60981588+Talha-crypto@users.noreply.github.com>
2022-04-21 23:28:50 +0500

    Token verification
    Verify token and allow user to make changes in credentials/private things

---
+++ middleware/requireLogin.js
@@ -0,0 +1,24 @@
+const Jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../keys')
+const mongoose=require('mongoose')
+const User= mongoose.model('User')
+
+//Function Checking token for allowing the private credentials of user
+module.exports=(req,res,next)=>{
+    const {authorization} =req.headers
+    // Authorization Bearer = Token given to user while logging in
+    if(!authorization){
+       return res.status(401).json({error:"You must be logged!"})
+    }
+    const token=authorization.replace("Bearer","")
+    jwt.verify(token,JWT_SECRET,(err,payload)=>{
+    if(err){
+        return res.status(401).json({error:"you must be logged in"})
+    }
+    const {_id} =payload
+    User.findone(_id).then(userdata=>{
+        req.user=userdata
+    })
+    next()
+    })
+}
--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,12 @@ const User=mongoose.model("User")
 const bcrypt=require("bcryptjs")
 const jwt=require("jsonwebtoken")
 const {JWT_SECRET}=require('../keys')
+const requireLogin= require('../middleware/requireLogin')
 
+//Function for accesing the credentials of user
+router.get('/protected',requireLogin,(req,res)=>{
+    res.send("Hello USer")
+})
 
 router.post('/signup',(req,res)=>{
     const {name,email,password} = req.body
