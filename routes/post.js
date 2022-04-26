9b7e0deb7313b34e01bf764ddf7b31f255012018
Talha-crypto <60981588+Talha-crypto@users.noreply.github.com>
2022-04-26 21:59:47 +0500

    Create Post method
    Creating post method and saving the post

--- routes/post.js
+++ routes/post.js
@@ -2,18 +2,24 @@ const express= require('express')
 const router= express.Router()
 const mongoose= require('mongoose')
 const requireLogin=require('../middleware/requireLogin')
-
+  
 router.post('/createpost',requireLogin,(req,res)=>{
     const {title,body}= req.body
     if(!title || !body){
         return res.status(422).json({error:"Post must have Title and Body"})
     }
-    consolle.log(req.user)
-        res.send("Ok")
-        // const post = Post({
-        //     title,
-        //     body,
-        // })
+    //Post saving func
+    const post =new post({
+            title,
+            body,
+            postedBy:req.user // How is posting the data will be retreived by this
+        })
+        post.save().then(result=>{
+            res.json({post:result})
+        })
+        .catch(err=>{
+            console.log(err)
+        })
 })
 
 module.exports=router
