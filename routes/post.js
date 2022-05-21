fcee187e18ce3a57812f4193dd69656348f8138b
Talha-crypto <60981588+Talha-crypto@users.noreply.github.com>
2022-05-21 23:01:29 +0500

    latest update

--- routes/post.js
+++ routes/post.js
@@ -2,24 +2,28 @@ const express= require('express')
 const router= express.Router()
 const mongoose= require('mongoose')
 const requireLogin=require('../middleware/requireLogin')
-  
+const Post=mongoose.model("Post")
+
 router.post('/createpost',requireLogin,(req,res)=>{
     const {title,body}= req.body
     if(!title || !body){
         return res.status(422).json({error:"Post must have Title and Body"})
     }
     //Post saving func
-    const post =new post({
-            title,
-            body,
-            postedBy:req.user // How is posting the data will be retreived by this
-        })
-        post.save().then(result=>{
-            res.json({post:result})
+    // console.log(req.user)
+    // res.send("okay")
+    req.user.password=undefined   // This done just for not storing the password in User's details of posted view in database
+    const posted =new Post({
+        title,
+        body,
+        postedBy:req.user
+    })
+    posted.save().then(result=>{
+        res.json({post:result})
         })
         .catch(err=>{
             console.log(err)
-        })
+    })
 })
 
 module.exports=router
