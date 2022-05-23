18a1ce1fd330d3b437348f586356fbd1192e28aa
Talha-crypto <60981588+Talha-crypto@users.noreply.github.com>
2022-05-23 23:33:21 +0500

    View Post
    View all the post of the user in mypost page

--- routes/post.js
+++ routes/post.js
@@ -37,4 +37,15 @@ router.post('/createpost',requireLogin,(req,res)=>{
     })
 })
 
+router.get('/mypost',requireLogin,(req,res)=>{
+    Post.find({postedBy:req.user._id})// this method is used to see post of user that is logged in
+    .populate("postedBy","_id name")
+    .then(mypost=>{
+        res.json({mypost})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
 module.exports=router
