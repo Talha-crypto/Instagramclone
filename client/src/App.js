e9ff8b6743d562f538c91861fbc1bd72655eb163
Talha-crypto <60981588+Talha-crypto@users.noreply.github.com>
2022-05-27 23:30:37 +0500

    Front End development
    Frontend under dev. Home, Profile, Sign-up, and Sign-in

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
-import React from "react"
-import './App.css';
+import React from "react";
+import './Components/Navbar'
+import "./App.css";
+import NavBar from "./Components/Navbar"
+import {BrowserRouter} from 'react-router-dom'
+import { Route } from "react-router-dom"
+import Home from './Components/screens/Home'
+import Profile from "./Components/screens/Profile"
+import Signin from "./Components/screens/Login"
+import Signup from "./Components/screens/Signup"
 
 function App() {
   return (
-    <h1>Hello</h1>
+      <BrowserRouter>
+        <NavBar />
+        <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/signin">
+            <Signin />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+      </BrowserRouter>
   );
 }
 
