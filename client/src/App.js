86724c803e60feebc9c9b08a5dc3924642c00474
Talha-crypto <60981588+Talha-crypto@users.noreply.github.com>
2022-07-01 21:17:14 +0500

    Routes updated
    Error in routes removed by using Routes from react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import './Components/Navbar'
 import "./App.css";
 import NavBar from "./Components/Navbar"
-import {BrowserRouter,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Components/screens/Home'
 import Profile from "./Components/screens/Profile"
 import Signin from "./Components/screens/Login"
@@ -10,26 +10,25 @@ import Signup from "./Components/screens/Signup"
 
 function App() {
   return (
-    <BrowserRouter>
-    <NavBar/>
-    
-    <Route path="/">
-      <Home />
-    </Route>
-    <Route path="/signup">
-      <Signup />
-    </Route>
-    <Route path="/signin">
-      <Signin />
-    </Route>
-    <Route path="/login">
-      <Signin />
-    </Route>
-    <Route path="/profile">
-      <Profile />
-    </Route>
 
-    </BrowserRouter>
+    <BrowserRouter>
+    <div className="max-w-screen-md mx-auto pt-20">
+      <NavBar />
+      <Routes>
+        <Route exact path="/" component={Home} />
+      </Routes>
+      <Routes>
+        <Route exact path="/login" component={Signin} />
+      </Routes>
+      <Routes>
+        <Route exact path="/profile" component={Profile} />
+      </Routes>
+      <Routes>
+        <Route exact path="/signup" component={Signup} />
+      </Routes>
+    </div>
+  </BrowserRouter>
+  
   );
 }
 
