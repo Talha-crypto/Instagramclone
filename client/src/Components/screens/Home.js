8e5e8bde4e4c2e55b027bf752633f7046ffb4209
Talha-crypto <60981588+Talha-crypto@users.noreply.github.com>
2022-07-03 11:31:56 +0500

    Homepage UI

--- client/src/Components/screens/Home.js
+++ client/src/Components/screens/Home.js
@@ -3,7 +3,15 @@ import React from 'react'
 
 const Home  = ()=> {
     return(
-        <h1>Home</h1>
+        <div className='home'>
+            <div className='card home-card'>
+                <h5>Ramsey Haiden</h5>
+                <div className='card-image'>
+                    <img src='https://images.unsplash.com/photo-1655791378082-b47ed92ee363?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM0fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'/>
+                </div>
+                
+            </div>
+        </div>
     )
 }
 
