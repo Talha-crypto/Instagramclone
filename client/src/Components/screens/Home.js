510ca13de2100d8732c5ebd8c6690f2ce27f77f5
Talha-crypto <60981588+Talha-crypto@users.noreply.github.com>
2022-07-03 15:28:08 +0500

    Static Homepage
    Home page static setup.

--- client/src/Components/screens/Home.js
+++ client/src/Components/screens/Home.js
@@ -5,11 +5,52 @@ const Home  = ()=> {
     return(
         <div className='home'>
             <div className='card home-card'>
-                <h5>Ramsey Haiden</h5>
+                <strong><h5>Ramsey Haiden</h5></strong>
                 <div className='card-image'>
-                    <img src='https://images.unsplash.com/photo-1655791378082-b47ed92ee363?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM0fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'/>
+                    <img src='https://images.unsplash.com/photo-1655791378082-b47ed92ee363?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM0fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='homepage posts'/>
                 </div>
-                
+                <div className='card-content'>
+                <i class="material-icons" style={{color:"red"}}>favorite_border</i>
+                    <h6>Post title</h6>
+                    <p>Description of Post</p>
+                    <input type="text"placeholder='add a comment'  />
+                </div>
+            </div>
+            <div className='card home-card'>
+                <strong><h5>Ramsey Haiden</h5></strong>
+                <div className='card-image'>
+                    <img src='https://images.unsplash.com/photo-1655791378082-b47ed92ee363?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM0fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='homepage posts'/>
+                </div>
+                <div className='card-content'>
+                <i class="material-icons" style={{color:"red"}}>favorite_border</i>
+                    <h6>Post title</h6>
+                    <p>Description of Post</p>
+                    <input type="text"placeholder='add a comment'  />
+                </div>
+            </div>
+            <div className='card home-card'>
+                <strong><h5>Ramsey Haiden</h5></strong>
+                <div className='card-image'>
+                    <img src='https://images.unsplash.com/photo-1655791378082-b47ed92ee363?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM0fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='homepage posts'/>
+                </div>
+                <div className='card-content'>
+                <i class="material-icons" style={{color:"red"}}>favorite_border</i>
+                    <h6>Post title</h6>
+                    <p>Description of Post</p>
+                    <input type="text"placeholder='add a comment'  />
+                </div>
+            </div>
+            <div className='card home-card'>
+                <strong><h5>Ramsey Haiden</h5></strong>
+                <div className='card-image'>
+                    <img src='https://images.unsplash.com/photo-1655791378082-b47ed92ee363?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM0fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='homepage posts'/>
+                </div>
+                <div className='card-content'>
+                <i class="material-icons" style={{color:"red"}}>favorite_border</i>
+                    <h6>Post title</h6>
+                    <p>Description of Post</p>
+                    <input type="text"placeholder='add a comment'  />
+                </div>
             </div>
         </div>
     )
