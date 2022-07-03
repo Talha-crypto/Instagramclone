4d18835f515698bf3af6c8d5606930a3d2f8f5b3
Talha-crypto <60981588+Talha-crypto@users.noreply.github.com>
2022-07-03 15:34:23 +0500

    createpost screeen
    Create post routes Nodejs route settings

---
+++ client/src/Components/screens/Createpost.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Createpost=()=>{
+    <div className="card input-filed">
+        <input type="text" placeholder="title"/>
+        <input type="text" placeholder="body"/>
+
+    </div>
+}
+
+export default Createpost
--- client/src/Components/screens/Home.js
+++ client/src/Components/screens/Home.js
@@ -10,7 +10,7 @@ const Home  = ()=> {
                     <img src='https://images.unsplash.com/photo-1655791378082-b47ed92ee363?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM0fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='homepage posts'/>
                 </div>
                 <div className='card-content'>
-                <i class="material-icons" style={{color:"red"}}>favorite_border</i>
+                <i className="material-icons" style={{color:"red"}}>favorite_border</i>
                     <h6>Post title</h6>
                     <p>Description of Post</p>
                     <input type="text"placeholder='add a comment'  />
@@ -22,7 +22,7 @@ const Home  = ()=> {
                     <img src='https://images.unsplash.com/photo-1655791378082-b47ed92ee363?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM0fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='homepage posts'/>
                 </div>
                 <div className='card-content'>
-                <i class="material-icons" style={{color:"red"}}>favorite_border</i>
+                <i className="material-icons" style={{color:"red"}}>favorite_border</i>
                     <h6>Post title</h6>
                     <p>Description of Post</p>
                     <input type="text"placeholder='add a comment'  />
@@ -34,7 +34,7 @@ const Home  = ()=> {
                     <img src='https://images.unsplash.com/photo-1655791378082-b47ed92ee363?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM0fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='homepage posts'/>
                 </div>
                 <div className='card-content'>
-                <i class="material-icons" style={{color:"red"}}>favorite_border</i>
+                <i className="material-icons" style={{color:"red"}}>favorite_border</i>
                     <h6>Post title</h6>
                     <p>Description of Post</p>
                     <input type="text"placeholder='add a comment'  />
@@ -46,7 +46,7 @@ const Home  = ()=> {
                     <img src='https://images.unsplash.com/photo-1655791378082-b47ed92ee363?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM0fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='homepage posts'/>
                 </div>
                 <div className='card-content'>
-                <i class="material-icons" style={{color:"red"}}>favorite_border</i>
+                <i className="material-icons" style={{color:"red"}}>favorite_border</i>
                     <h6>Post title</h6>
                     <p>Description of Post</p>
                     <input type="text"placeholder='add a comment'  />
