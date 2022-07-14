c1a5438352e1c08b173f027913aca3211ac324b9
Talha-crypto <60981588+Talha-crypto@users.noreply.github.com>
2022-07-14 11:51:12 +0500

    Create Post

--- client/src/Components/screens/Createpost.js
+++ client/src/Components/screens/Createpost.js
@@ -1,9 +1,13 @@
-import React,{useState} from "react";
+import React,{useState} from "react"
+import M from "materialize-css"
+import { useNavigate } from "react-router-dom"
 
 const Createpost = () =>{
+  const navigate=useNavigate()
   const [title,setTitle]=useState()
   const [body,setBody]=useState()
   const [image,setImage]=useState()
+  const [url,setUrl]=useState()
 
   const postDetails= ()=>{
     const data = new FormData()
@@ -16,12 +20,34 @@ const Createpost = () =>{
       body:data})
     .then(res=>res.json())
     .then(data=>{
-      console.log(data)
+      setUrl(data.url)
     })
     .catch(err=>{
       console.log(err)
     })
-  }
+
+    fetch("http://localhost:5000/createpost",{  // this causes error when tries to reach the sigup mathod. because both apps are running on different servers. to resolve this issue we make use of Proxying Api Requests in Development.
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },body:JSON.stringify({
+                title,
+                body,
+                pic:url
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+            if(data.error){
+                M.toast({html: data.error,classes:"#c62828 red darken-3"})
+            }
+            else{
+                M.toast({html:"Posted Successfuly",classes:"#2e7d32 green darken-3"})
+                navigate('/')  //The useHistory hook gives you access to the history instance that you may use to navigate.
+            }
+        }).catch(err=>{
+         console.log(err)
+        })
+       }
 
     return(<div className="card input-filed" 
     style={{
@@ -54,10 +80,9 @@ const Createpost = () =>{
       </div>
     </div>
     <button className="btn waves-effect waves-light #64b5f6 light-blue darken-1"
-    onClick={()=>postDetails()}
-    >
-                    Submit post
-                </button>
+    onClick={()=>postDetails()} >
+    Submit post
+    </button>
 
     </div>)
 }
--- routes/post.js
+++ routes/post.js
@@ -16,8 +16,8 @@ router.get('/allpost',(req,res)=>{
 })
 
 router.post('/createpost',requireLogin,(req,res)=>{
-    const {title,body}= req.body
-    if(!title || !body){
+    const {title,body,picurl}= req.body
+    if(!title || !body || picurl){  // picurl is the link that cloudinary will provide after image gets uploaded there.
         return res.status(422).json({error:"Post must have Title and Body"})
     }
     //Post saving func
@@ -27,6 +27,7 @@ router.post('/createpost',requireLogin,(req,res)=>{
     const posted =new Post({
         title,
         body,
+        picurl,
         postedBy:req.user
     })
     posted.save().then(result=>{
